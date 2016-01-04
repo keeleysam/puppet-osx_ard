aed95bb830726f68375831cd654500ab50f400aa
Samuel Keeley <sam.keeley@gmail.com>
2016-01-04 14:32:54 -0600

    change to use operatingsystemmajrelease fact

--- manifests/kickstart/enable.pp
+++ manifests/kickstart/enable.pp
@@ -2,7 +2,7 @@
 class osx_ard::kickstart::enable {
   $kickstart = $osx_ard::kickstart
 
-  if $::macosx_productversion_major >= '10.11' {
+  if $::operatingsystemmajrelease >= '15' {
     exec { 'Kickstart ARD' :
       command => "${kickstart} -activate -configure -allowAccessFor -specifiedUsers",
       unless  => '/bin/cat /Library/Application\ Support/Apple/Remote\ Desktop/RemoteManagement.launchd | /usr/bin/grep -q enabled',
